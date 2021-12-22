86297d4d3d97d3b68dadf650a4d49b35f4bc7018
Víctor Oliva <olivarra1@gmail.com>
2021-12-22 19:44:13 +0100

    import basic font, print text to screen

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Point};
 use std::time::{Duration, Instant};
 
-use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Point};
+mod font;
 
 const SCALE: f32 = 4.0;
 const WIDTH: u32 = 128;
 const HEIGHT: u32 = 128;
-const FPS: u32 = 2000;
+const FPS: u32 = 30;
 const NANOS_IN_SEC: u32 = 1_000_000_000;
 const TARGET_NANOS: u32 = NANOS_IN_SEC / FPS;
 
@@ -17,17 +18,18 @@ fn main() -> Result<(), String> {
         .window(
             "rust-sdl2 demo",
             (WIDTH as f32 * SCALE) as u32,
-            (HEIGHT as f32 * SCALE) as u32,
+            (HEIGHT as f32 * SCALE) as u32 + 2, // MacOS eats the first line, visible when scale=1
         )
         .position_centered()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
-    canvas.set_scale(SCALE, SCALE)?;
+    canvas.set_logical_size(WIDTH, HEIGHT).unwrap(); // Works better than set_scale
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     let mut fps_meter = FpsMeter::new();
+    let mut last_fps = "".to_string();
     'running: loop {
         let render_start = Instant::now();
 
@@ -46,14 +48,33 @@ fn main() -> Result<(), String> {
         // The rest of the game loop goes here...
 
         canvas.set_draw_color(Color::RGB(255, 210, 0));
-        for x in 0..WIDTH {
-            canvas.draw_point(Point::new(x as i32, 0 as i32))?;
-            canvas.draw_point(Point::new(x as i32, (HEIGHT - 1) as i32))?;
+        font::print(
+            &mut canvas,
+            (WIDTH as i32) - (last_fps.len() * 4) as i32,
+            0,
+            &last_fps,
+        )?;
+
+        // for x in 0..WIDTH {
+        //     canvas.draw_point(Point::new(x as i32, 0 as i32))?;
+        //     canvas.draw_point(Point::new(x as i32, (HEIGHT - 1) as i32))?;
+        // }
+        // for y in 0..HEIGHT {
+        //     canvas.draw_point(Point::new(0 as i32, y as i32))?;
+        //     canvas.draw_point(Point::new((WIDTH - 1) as i32, y as i32))?;
+        // }
+        for line in 0..HEIGHT / 6 {
+            font::print(
+                &mut canvas,
+                0,
+                (line * 6) as i32,
+                "the quick brown fox jumps over",
+            )?;
         }
-        for y in 0..HEIGHT {
-            canvas.draw_point(Point::new(0 as i32, y as i32))?;
-            canvas.draw_point(Point::new((WIDTH - 1) as i32, y as i32))?;
-        }
+        // font::print(&mut canvas, 0, 10, "THE QUICK BROWN FOX JUMPS OVER")?;
+        // font::print(&mut canvas, 10, 16, "THE LAZY DOG")?;
+        // font::print(&mut canvas, 0, 22, "the quick brown fox jumps over")?;
+        // font::print(&mut canvas, 0, 28, "the lazy dog")?;
 
         canvas.present();
 
@@ -63,7 +84,7 @@ fn main() -> Result<(), String> {
         }
 
         if let Some(_fps) = fps_meter.register_frame() {
-            println!("{}", _fps);
+            last_fps = format!("{}", _fps);
         }
     }
 
